4153f58002ff1226fc395e1ca6d11fc61dcd2c0d
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-09-24 13:34:43 +0100

    jquery added

---
+++ test-zurich/scripts/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  setTimeout(function() {
+    $('.heading').css({'display':'block'});
+  }, 1000);
+  setTimeout(function() {
+    $('.content').css({'display':'block'});
+  }, 2000);
+  setTimeout(function() {
+    $('.tagline').css({'display':'block'});
+  }, 3000);
+  setTimeout(function() {
+    $('.logo').css({'display':'block'});
+  }, 4000);
+  setTimeout(function() {
+    $('.cta').css({'display':'block'});
+  }, 5000);
+});
+   // jQuery methods go here...
